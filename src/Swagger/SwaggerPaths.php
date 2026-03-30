f8437dcb394406d28c6d4616310b95fb07a767a0
tangwei <tw2066@163.com>
2026-03-30 21:01:28 +0800

    optimized code

--- src/Swagger/SwaggerPaths.php
+++ src/Swagger/SwaggerPaths.php
@@ -30,7 +30,7 @@ class SwaggerPaths
 
     protected array $classMethodArray = [];
 
-    protected array $operationIds = [];
+    protected static array $operationIds = [];
 
     public function __construct(
         public string $serverName,
@@ -148,13 +148,13 @@ class SwaggerPaths
      */
     protected function getOperationId(string $route, string $methods): string
     {
-        $route = str_replace(['{', '}'], '', $route);
+        $route = str_replace(['{','}'], '', $route);
         $operationId = Str::camel(str_replace('/', '_', $route));
         if (empty($operationId)) {
             $operationId = '-';
         }
-        if (! isset($this->operationIds[$operationId])) {
-            $this->operationIds[$operationId] = true;
+        if (! isset(self::$operationIds[$operationId])) {
+            self::$operationIds[$operationId] = [$route, $methods];
             return $operationId;
         }
         return $this->getOperationId($operationId . ucfirst(strtolower($methods)), $methods);
